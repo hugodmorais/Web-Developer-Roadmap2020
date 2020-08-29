8ac39efd9b8a5adb750637923562ea3dc3a05e4c
hugodmorais <hugodanielmorais@gmail.com>
2020-08-29 15:40:13 +0000

    Create component Card

--- React_Redux/robofriends/src/index.js
+++ React_Redux/robofriends/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import Card from './Card';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+import {robots} from './robots';
 
 ReactDOM.render(
-  <h1><Hello greeting={'Hello' + 'React Ninja'}/></h1>,
-  document.getElementById('root')
+                <div>
+                  <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+                  <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+                  <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+                </div>
+  , document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
