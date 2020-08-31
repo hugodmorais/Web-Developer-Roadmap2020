8cc4afeba1228a76e5de43d17a56d10a6f11f264
hugodmorais <hugodanielmorais@gmail.com>
2020-08-31 22:39:20 +0000

    Updates

---
+++ precache-manifest.569bdb23efd6503292a28b0e6bb58f20.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "821202303a8845f2ed7e2ce7a7e452e0",
+    "url": "/Web-Developer-Roadmap2020/tree/master/React_Redux/robofriends/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/Web-Developer-Roadmap2020/tree/master/React_Redux/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "7597418b533d839f62d1",
+    "url": "/Web-Developer-Roadmap2020/tree/master/React_Redux/robofriends/static/css/main.d95dc5c7.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/Web-Developer-Roadmap2020/tree/master/React_Redux/robofriends/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Web-Developer-Roadmap2020/tree/master/React_Redux/robofriends/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7597418b533d839f62d1",
+    "url": "/Web-Developer-Roadmap2020/tree/master/React_Redux/robofriends/static/js/main.9ba6a8f8.chunk.js"
+  },
+  {
+    "revision": "bf9c6a600e2a33d7497c",
+    "url": "/Web-Developer-Roadmap2020/tree/master/React_Redux/robofriends/static/js/runtime-main.140279a6.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/Web-Developer-Roadmap2020/tree/master/React_Redux/robofriends/static/media/SEGA.82e09f78.TTF"
+  }
+]);
