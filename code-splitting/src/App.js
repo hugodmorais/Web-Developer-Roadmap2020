c525cc7193ab888b77556c6d7fab540043cfada8
hugodmorais <hugodanielmorais@gmail.com>
2020-09-14 22:59:43 +0000

    create pages, buttons and style

--- code-splitting/src/App.js
+++ code-splitting/src/App.js
@@ -1,19 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>Welcome to React</div>
-      </header>
-      <Page1 />
-      <Page2 />
-      <Page3 />
-    </div>
-  );
+import Page1 from './components/Page1';
+import Page2 from './components/Page2';
+import Page3 from './components/Page3';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      route: 'page1'
+    }
+  }
+
+  onRouteChange = (route) => {
+    this.setState({route: route});
+  }
+
+  render() {
+    if (this.state.route === 'page1') {
+      return <Page1 onRouteChange={this.onRouteChange} />
+    } else if (this.state.route === 'page2') {
+      return <Page2 onRouteChange={this.onRouteChange} />
+    } else if (this.state.route === 'page3') {
+      return <Page3 onRouteChange={this.onRouteChange} />
+    }
+  }
 }
 
 export default App;
